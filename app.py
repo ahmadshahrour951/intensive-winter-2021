5b27c07f4e79789d4f074dd9ccc5ad1c45635cf8
shahrour <ahmadshahrour951@gmail.com>
2021-01-11 21:07:31 -0800

    actions skeleton pages

--- app.py
+++ app.py
@@ -51,10 +51,53 @@ def aspirations_delete(id):
   return jsonify(success=True)
 
 
+@app.route('/actions/<id>', methods=['GET'])
+def actions_detail_render(id):
+  main_data = list(mongo.db.actions.find({})) or []
+
+  for data in main_data:
+      data['_id'] = str(data['_id'])
+
+  detail_data = mongo.db.actions.find_one({'_id': ObjectId(id)})
+
+  return render_template('base.jinja2', page="actions", main_data=main_data, detail_data=detail_data, is_detail=True)
+
 @app.route('/actions', methods=['GET'])
 def actions_render():
-    return render_template('base.jinja2', page="actions")
+  main_data = list(mongo.db.actions.find({})) or []
 
+  for data in main_data:
+      data['_id'] = str(data['_id'])
+  
+  return render_template('base.jinja2', page="actions", main_data=main_data, is_detail=False)
+
+
+@app.route('/actions', methods=['POST', 'PATCH'])
+def actions_post():
+  actions = mongo.db.actions
+  http_method = request.form.get('_method')
+
+  if http_method == 'post':
+    actions.insert_one({
+        "name": request.form.get('name'),
+        "description": request.form.get('description')
+    })
+  elif http_method == 'patch':
+    actions.update_one({'_id': ObjectId(request.form.get('_id'))},
+                           {
+        '$set': {
+            'name': request.form.get('name'),
+            'description': request.form.get('description')
+        }
+    })
+
+  return redirect(url_for('actions_render'))
+
+@app.route('/actions/<id>', methods=['DELETE'])
+def actions_delete(id):
+  actions = mongo.db.actions
+  actions.delete_one({'_id': ObjectId(id)})
+  return jsonify(success=True)
 
 if __name__ == "__main__":
     app.run(debug=True)
